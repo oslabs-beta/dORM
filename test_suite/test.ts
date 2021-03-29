aa2f3b810ddf359f0ff6511a19fc734a80fe09de
m-aung <myothuwinaung1991@gmail.com>
2021-03-29 14:26:16 -0400

    test cases updated: insert,select,update

--- test_suite/test.ts
+++ test_suite/test.ts
@@ -1,6 +1,6 @@
-import {Dorm } from '../lib/draft.ts';
-import { assertEquals, assertNotEquals, Pool, PostgresError, PoolClient } from "../deps.ts";
-import { template } from '../lib/sql-template.ts';
+import { Dorm } from '../lib/draft.ts';
+import { assertEquals, assertNotEquals} from "../deps.ts";
+// import { template } from '../lib/sql-template.ts';
 // add url in test_url.ts file
 import {url} from './test_url.ts'
 /*------------ CONNECTING TO DATABASE ------------*/
@@ -15,78 +15,25 @@ interface Userprofile{
   created_on :string,
   last_login :string 
 }
-
-/*------------ TESTING SELECT METHOD ------------*/
-const selectQuery = await dorm.select()
-.from('userprofile')
-.where('user_id=1').then((data: any) => {
-  // console.log('Returned data:',data.rows);
-  return data.rows;
-})
-Deno.test(`Is "SELECT" method working:`, () => {
-  assertNotEquals(selectQuery,undefined, 'Output is not undefined.')
-  // function queryResult(query: Userprofile):number {
-  //   let length = 0;
-  //   for(const properties in query){
-  //     length+= 1;
-  //   }
-  //   return length;
-  // }
-  // queryResult(selectQuery);
-  // console.log(queryResult);
-  // assertEquals(queryResult[0]["user_id"], "1", 'Output has at least one row');
-});
-Deno.test(`Is single column selecting in "SELECT" working:`,  async () => {
-  const test = await dorm.select('username').from('userprofile').where('user_id = 2').then((data: any) => {
-    return data.rows;
-  });
-  // assertEquals(test.info.action.type , 'SELECT', 'Type is changed to SELECT');
-  // assertEquals(test.info.action.columns , 'username', 'column is changed to username');
-  // assertEquals(test.info.action.table , 'userprofile', 'table is changed to userprofile');
-  // assertEquals(test.info.action.type , null, 'Type is reset');
-  // assertEquals(test.info.action.columns , '*', 'column is reset');
-  // assertEquals(test.info.action.table , null, 'table is reset');
-})
-
-Deno.test(`Is multiple column selecting in "SELECT" working:`,  async () => {
-  const test =  await dorm.select('username, email').from('userprofile').where('user_id = 3').then((data: any) => {
-    return data.rows;
-  });
-  // assertEquals(test.info.action.type , 'SELECT', 'Type is changed to SELECT');
-  // assertEquals(test.info.action.columns , 'username, email', 'column is changed to username, email');
-  // assertEquals(test.info.action.table , 'userprofile', 'table is changed to userprofile');
-  // assertEquals(test.info.action.type , null, 'Type is reset');
-  // assertEquals(test.info.action.columns , '*', 'column is reset');
-  // assertEquals(test.info.action.table , null, 'table is reset');
-})
-
-/*------------ TESTING INSERT METHOD ------------*/
-
-const insertQuery = await dorm.insert([{'username':'newDogs', 'password': 'new99', 'email': 'newdog@dog.com','created_on':'NOW()' }])
-.from('userprofile')
-.returning().then((data: any) => {
-   return data;
- })
- const insertSelectQuery1 = await dorm.select()
-.from('userprofile')
-.then((data: any) => {
-  // console.log('Returned data:',data.rows);
-  return data.rows;
-})
-
- Deno.test(`Is "INSERT" method working:`, () => {
-  assertNotEquals(insertQuery, undefined, 'Output is not undefined.')
-  assertNotEquals(insertSelectQuery1, selectQuery, 'Output is not undefined.')
-});
-
-Deno.test(`Is single row inserting in "INSERT" working:`,  () => {
-  const test =  dorm.insert([{'username':'singleLady'}]).from('userprofile');
-  assertEquals(test.info.action.type , 'INSERT', 'Type is changed to INSERT');
-  assertEquals(test.info.action.columns , 'username', `column is changed to 'username'`);
-  assertEquals(test.info.action.values , `('singleLady')`, `value is changed to 'singleLady'`);
-  assertEquals(test.info.action.table , 'userprofile', 'table is changed to userprofile');
-  // the dorm class is not reset
-  test.info = {
+interface Info {
+  action: {
+    type: null | string;
+    table: null | string;
+    columns: null | string | string[];
+    values: string;
+  };
+  filter: {
+    where: boolean;
+    condition: null | string;
+  };
+  returning: {
+    active: boolean;
+    columns: string | string[];
+  };
+}
+// console.log(dorm);
+function toQueryString(arg:any){
+  arg.info = {
     action: {
       type: null,
       table: null,
@@ -102,15 +49,94 @@ Deno.test(`Is single row inserting in "INSERT" working:`,  () => {
       columns: '*',
     },
   };
+}
+/*------------ TESTING SELECT METHOD ------------*/
+var updateId = Math.floor(Math.random()*35);
+/*------------ TESTING SELECT METHOD ------------*/
+const selectQuery = await dorm.select()
+.from('userprofile')
+.where('user_id=1').then((data: any) => {
+  // console.log('Returned data:',data.rows);
+  return data.rows;
+}).catch((e)=> {throw e})
+Deno.test(`Is "SELECT" method working:`, () => {
+  assertNotEquals(selectQuery,undefined, 'Output is undefined.')
+});
+Deno.test(`Is single column selecting in "SELECT" working:`, () => {
+  const test = dorm.select('username').from('userprofile').where('user_id = 2')/*.then((data: any) => {
+    return data.rows;
+  })*/;
+  assertEquals(test.info.action.type , 'SELECT', 'Type is not changed to SELECT');
+  assertEquals(test.info.action.columns , 'username', 'Columns are not changed to username');
+  assertEquals(test.info.action.table , 'userprofile', 'Table is not changed to userprofile');
+  // reseting the info obj
+  toQueryString(test);
+  assertEquals(test.info.action.type , null, 'Type is not reset');
+  assertEquals(test.info.action.columns , '*', 'Columns are not reset');
+  assertEquals(test.info.action.table , null, 'Table is not reset');
+})
+
+Deno.test(`Is multiple column selecting in "SELECT" working:`,   () => {
+  const test =  dorm.select('username, email').from('userprofile')/*.where('user_id = 3').then((data: any) => {
+    return data.rows;
+  })*/;
+  assertEquals(test.info.action.type , 'SELECT', 'Type is not changed to SELECT');
+  assertEquals(test.info.action.columns , 'username, email', 'Columns are not changed to username, email');
+  assertEquals(test.info.action.table , 'userprofile', 'Table is not changed to userprofile');
+  // reseting the info obj
+  toQueryString(test);
+  assertEquals(test.info.action.type , null, 'Type is not reset');
+  assertEquals(test.info.action.columns , '*', 'Columns are not reset');
+  assertEquals(test.info.action.table , null, 'Table is not reset');
+})
+
+/*------------ TESTING INSERT METHOD ------------*/
+
+const insertQuery = await dorm.insert([{'username':'newDogs', 'password': 'new99', 'email': 'newdog@dog.com','created_on':'NOW()' }])
+.from('userprofile')
+.returning().then((data: any) => {
+  return data;
+})
+const insertSelectQuery1 = await dorm.select()
+.from('userprofile')
+.then((data: any) => {
+  // console.log('Returned data:',data.rows);
+  return data.rows;
+})
+
+Deno.test(`Is "INSERT" method working:`, () => {
+  assertNotEquals(insertQuery, undefined, 'Output is not undefined.')
+  assertNotEquals(insertSelectQuery1, selectQuery, 'Output is not undefined.')
+});
+
+Deno.test(`Is single row inserting in "INSERT" working:`,  () => {
+  const test =  dorm.insert([{'username':'singleLady'}]).from('userprofile');
+  assertEquals(test.info.action.type , 'INSERT', 'Type is not changed to INSERT');
+  assertEquals(test.info.action.columns , 'username', `Column is not changed to 'username'`);
+  assertEquals(test.info.action.values , `('singleLady')`, `Value is not changed to 'singleLady'`);
+  assertEquals(test.info.action.table , 'userprofile', 'Table is not changed to userprofile');
+  // reseting the info obj
+  toQueryString(test);
+  assertEquals(test.info.action.type , null, 'Type is not reset');
+  assertEquals(test.info.action.columns , '*', 'Columns are not reset');
+  assertEquals(test.info.action.values , '', 'Values are not reset');
+  assertEquals(test.info.action.table , null, 'Table is not reset');
+  
 });
 
 Deno.test(`Is multiple rows inserting in "INSERT" working:`,  () => {
-  const newTest =  dorm.insert([{'username':'tripleShot'}, {'username': 'halo'}]).from('userprofile');
+  const test =  dorm.insert([{'username':'tripleShot'}, {'username': 'halo'}]).from('userprofile');
   // console.log('from test:',newTest.info.action.values);
-  assertEquals(newTest.info.action.type , 'INSERT', 'Type is changed to INSERT');
-  assertEquals(newTest.info.action.columns , 'username', `column is changed to username`);
-  assertEquals(newTest.info.action.values , `('tripleShot'), ('halo')`, `value is changed to 'tripleShot, halo'`);
-  assertEquals(newTest.info.action.table , 'userprofile', 'table is changed to userprofile');
+  assertEquals(test.info.action.type , 'INSERT', 'Type is not changed to INSERT');
+  assertEquals(test.info.action.columns , 'username', `Columns are not changed to username`);
+  assertEquals(test.info.action.values , `('tripleShot'), ('halo')`, `Value is not changed to 'tripleShot, halo'`);
+  assertEquals(test.info.action.table , 'userprofile', 'Table is not changed to userprofile');
+  // reseting the info obj
+  toQueryString(test);
+  assertEquals(test.info.action.type , null, 'Type is not reset');
+  assertEquals(test.info.action.columns , '*', 'Columns are not reset');
+  assertEquals(test.info.action.values , '', 'Values are not reset');
+  assertEquals(test.info.action.table , null, 'Table is not reset');
 });
 
 
@@ -118,38 +144,64 @@ Deno.test(`Is multiple rows inserting in "INSERT" working:`,  () => {
 
 const testUpdateQuery1 = await dorm.select()
 .from('userprofile')
-.where('user_id=1').then((data: any) => {
+.where(`user_id=${updateId}`).then((data: any) => {
   // console.log('Returned data:',data.rows);
   return data.rows;
 })
-const updateQuery = await dorm.update({'username':'newDogs', 'password': 'iLoveDogs'}).where('user_id = 1')
+const updateQuery = await dorm.update({'username':'newDogs', 'password': 'iLoveDogs'}).where(`user_id = ${updateId}`)
 .from('userprofile')
 .returning().then((data: any) => {
   // console.log(data.rows);
-   return data;
- })
+  return data;
+})
 
- Deno.test(`Is "UPDATE" method working for one row update:`, () => {
+Deno.test(`Is "UPDATE" method working for one row update:`, () => {
+  const test = dorm.update({'username':'newDogs', 'password': 'iLoveDogs'}).where(`user_id = ${updateId+1}`)
+  .from('userprofile')
+  .returning()
   assertNotEquals(updateQuery, undefined, 'Output is not undefined.')
-  assertNotEquals(updateQuery, testUpdateQuery1 , 'Output has at least one row');
+  assertNotEquals(updateQuery, testUpdateQuery1 , 'Output has more than one row');
+  assertEquals(test.info.action.type , 'UPDATE', 'Type is not changed to UPDATE');
+  assertEquals(test.info.action.columns , `username = 'newDogs', password = 'iLoveDogs'`, `column is not changed to username = 'newDogs', password = 'iLoveDogs'`);
+  assertEquals(test.info.action.table , 'userprofile', 'table is not changed to userprofile');
+   // reseting the info obj
+   toQueryString(test);
+   assertEquals(test.info.action.type , null, 'Type is not reset');
+   assertEquals(test.info.action.columns , '*', 'Columns are not reset');
+   assertEquals(test.info.action.values , '', 'Values are not reset');
+   assertEquals(test.info.action.table , null, 'Table is not reset');
+  
 });
 
 const testUpdateQuery2 = await dorm.select()
 .from('userprofile')
-.where('user_id <= 1').then((data: any) => {
+.where(`user_id <= ${updateId}`).then((data: any) => {
   // console.log('Returned data:',data.rows);
   return data.rows;
 })
 
-const eightRowsUpdateQuery = await dorm.update({'username':'Dogs', 'password': 'ihave8Dogs'}).where('user_id <= 8')
+const eightRowsUpdateQuery = await dorm.update({'username':'Dogs', 'password': 'ihave8Dogs'})
 .from('userprofile')
+.where(`user_id <= ${updateId}`)
 .returning().then((data: any) => {
   // console.log(data.rows);
-   return data;
- })
+  return data;
+})
 
- Deno.test(`Is "UPDATE" method working for multiple rows update:`, () => {
-  assertNotEquals(eightRowsUpdateQuery, testUpdateQuery2, 'Exactly updated 8 rows');
+Deno.test(`Is "UPDATE" method working for multiple rows update:`, () => {
+  const test = dorm.update({'username':'Dogs', 'password': 'ihave8Dogs'}).where(`user_id <= ${updateId}`)
+  .from('userprofile')
+  .returning();
+  assertNotEquals(eightRowsUpdateQuery, testUpdateQuery2, `Exactly updated ${updateId} rows`);
+  assertEquals(test.info.action.type , 'UPDATE', 'Type is not changed to UPDATE');
+  // console.log('...................................',test.info.action.columns);
+  assertEquals(test.info.action.columns , `username = 'Dogs', password = 'ihave8Dogs'`, `column is not changed to username = 'Dogs', password = 'ihave8Dogs'`);
+  assertEquals(test.info.action.table , 'userprofile', 'Table is not changed to userprofile');
+  // reseting the info obj
+  toQueryString(test);
+  assertEquals(test.info.action.type , null, 'Type is not reset');
+  assertEquals(test.info.action.columns , '*', 'Columns are not reset');
+  assertEquals(test.info.action.table , null, 'Table is not reset');
 });
 
 const testUpdateQuery3 = await dorm.select()
@@ -162,25 +214,58 @@ const allRowsUpdateQuery = await dorm.update({'username':'restarted', 'password'
 .from('userprofile')
 .returning().then((data: any) => {
   // console.log(data.rows);
-   return data;
- })
- Deno.test(`Is "UPDATE" method working for all rows update:`, () => {
+  return data;
+})
+Deno.test(`Is "UPDATE" method working for all rows update:`, () => {
+  const test = dorm.update({'username':'restarted', 'password': 'iamADog'})
+  .from('userprofile')
+  .returning();
   assertNotEquals(allRowsUpdateQuery, testUpdateQuery3, 'The whole table was not updated');
+  assertEquals(test.info.action.type , 'UPDATE', 'Type is not changed to UPDATE');
+  assertEquals(test.info.action.columns , `username = 'restarted', password = 'iamADog'`, `column is not changed to username = 'restarted', password = 'iamADog'`);
+  assertEquals(test.info.action.table , 'userprofile', 'Table is not changed to userprofile');
+  // reseting the info obj
+  toQueryString(test);
+  assertEquals(test.info.action.type , null, 'Type is not reset');
+  assertEquals(test.info.action.columns , '*', 'Columns are not reset');
+  assertEquals(test.info.action.values , '', 'Values are not reset');
+  assertEquals(test.info.action.table , null, 'Table is not reset');
+  
 });
 
 /*------------ TESTING DELETE METHOD ------------*/
 
-const deleteOneQuery = await dorm.delete().from('userprofile').where('user_id = 20').returning().then(data=> {
-  return data.row[0];
-});
-
 const testDeleteQuery1 = await dorm.select()
 .from('userprofile')
-.where('user_id = 20').then((data: any) => {
-  // console.log('Returned data:',data.rows);
+.where(`user_id = ${updateId}`).then((data: any) => {
+  // console.log('Before deleting data:',data.rows);
+  return data.rows;
+})
+const deleteOneQuery1 = await dorm.delete().from('userprofile').where(`user_id = ${updateId}`).returning().then(data=> {
+  return data;
+});
+// console.log(deleteOneQuery)
+
+const testDeleteQuery2 = await dorm.select()
+.from('userprofile')
+.where(`user_id = ${updateId}`).then((data: any) => {
+  // console.log('After deleted data:',data.rows);
   return data.rows;
 })
 
-Deno.test(`Is Delete single row working:`, ()=> {
-  assertNotEquals(deleteOneQuery, undefined,'Delete is not completed!')
+Deno.test(`Is Delete single row working at id ${updateId+1}:`, ()=> {
+  const test = dorm.delete()
+  .from('userprofile')
+  .where(`user_id = ${updateId+1}`).returning();
+  assertNotEquals(deleteOneQuery1, undefined,'Delete is not completed!');
+  // reseting the info obj
+  toQueryString(test)
+  assertEquals(test.info.action.type , null, 'Type is not reset');
+  assertEquals(test.info.action.columns , '*', 'Column are not reset');
+  assertEquals(test.info.action.values , '', 'Values are not reset');
+  assertEquals(test.info.action.table , null, 'Table is not reset');
 })
+
+const deleteOneQuery2 = await dorm.delete().from('userprofile').where(`user_id = ${updateId}`).returning().then(data=> {
+  return data;
+});
