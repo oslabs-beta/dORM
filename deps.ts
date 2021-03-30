df47f765e5c073128192ff363c41f954a9849710
chaekmh <chaekmh@gmail.com>
2021-03-30 12:13:29 -0400

    Revert "Testing files created for SELECT, INSERT, DELETE, UPDATE  (#4)"
    This reverts commit b67b46833e4954af358d3001c6cda5e82db66895.

--- deps.ts
+++ deps.ts
@@ -1,7 +1,3 @@
 // DENO-POSTGRES dependecies --------------------------------------------------
 export { Pool, PostgresError } from 'https://deno.land/x/postgres/mod.ts';
 export { PoolClient } from 'https://deno.land/x/postgres@v0.8.0/client.ts';
-export {
-  assertEquals,
-  assertNotEquals,
-} from "https://deno.land/std@0.91.0/testing/asserts.ts";
