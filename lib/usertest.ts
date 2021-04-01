d427c391ccdf93a98f6479dd7fde208feb4c436b
Nick Stillman <nick.edward.stillman@gmail.com>
2021-04-01 11:04:50 -0400

    latest

--- lib/usertest.ts
+++ lib/usertest.ts
@@ -57,78 +57,6 @@ const dorm = new Dorm(url);
 // console.log('My Test Query:', testQuery);
 
 // **** INSERT
-<<<<<<< HEAD
-// try {
-//   const testQuery: any = await dorm
-//   // .insert([
-//   //   { name: 'hantest2', gender: 'male' },
-//   //   { name: 'hanjitest2', hair_color: 'sexy' },
-//   //   {
-//   //     hair_color: 'tits',
-//   //     eye_color: 'rainbow',
-//   //     name: 'nicktest2',
-//   //     height: 99,
-//   //   },
-//   // ])
-//   .insert([{name: 'Elvis'}, {name: 'Lebowski', hair_color: undefined}])
-//   .from('people')
-//   .returning('name')
-//   .toString();
-//     // .then((data: any) => {
-//     //   console.log('Our then');
-//     //   return data.rows;//[0];
-//     // })
-//     // .then((data) => {
-//     //   console.log('Bult-in then: ', data);
-//     //   return data;
-//     // })
-//     // .catch(e => console.log('.catch error:', e));
-    
-//     console.log('testQuery:', testQuery.rows[0])
-    
-//   } catch (e) {
-    
-//     console.log('e:', e)
-//   }
-  
-  
-  
-  // **** SELECT #2
-  
-  // const testQuery = await dorm
-  //    .select()
-  //    .table('people')
-  //    .then((data: any) => {
-  //      console.log('Our then');
-  //      return data.rows[0];
-  //    })
-  //    .then((data) => {
-  //      console.log('Bult-in then: ', data);
-  //      return data;
-  //    })
-  //    .catch((e) => console.log('ERRRRRRRRRRRR', e));
-  //  console.log('My Test Query:', testQuery);
-  
-  
-  // const testQuery: any = await dorm
-  // .select() //'*')
-  // .select()
-  // .from('people')
-  // .where('_id = 1')
-  // .then((data: any) => {
-  //   console.log('first then');
-  //   return data.rows[0];
-  // })
-  //   .then((data: any) => {
-  // // throw Error('test err');
-  //     console.log('promise then: ', data);
-  //     return data;
-  //   })
-  // .catch(e => console.log('.catch error:', e));
-  
-  
-  // console.log('testQuery:', testQuery.rows[0]);
-=======
 
 // const testQuery = await dorm
 // // .insert([
@@ -191,4 +119,3 @@ const dorm = new Dorm(url);
 
 
 // console.log('testQuery:', testQuery.rows[0]);
->>>>>>> main
