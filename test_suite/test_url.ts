575856e379117c30f0be82d651e7e312626725cd
Myo <myothuwinaung1991@gmail.com>
2021-04-08 17:58:27 -0400

    Delete test_url.ts

--- test_suite/test_url.ts
+++
@@ -1,2 +0,0 @@
-export const url = "postgres://vlayinbm:NANiFUh87QYEgyrfgy_qiYdfAU00HWsc@queenie.db.elephantsql.com:5432/vlayinbm"
-// psql -d "postgres://vlayinbm:NANiFUh87QYEgyrfgy_qiYdfAU00HWsc@queenie.db.elephantsql.com:5432/vlayinbm" -f ./test_suite/db_test_postgres_create.sql
