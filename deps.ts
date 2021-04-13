31bef07c8ed8e7e4cdf13867a27e6c14824dd632
Nick Stillman <nick.edward.stillman@gmail.com>
2021-04-13 13:36:15 -0400

    latest

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 // DENO-POSTGRES dependecies --------------------------------------------------
 export { Pool, PostgresError } from 'https://deno.land/x/postgres/mod.ts';
-export { PoolClient } from 'https://deno.land/x/postgres/client.ts';
+export { PoolClient } from 'https://deno.land/x/postgres@v0.8.0/client.ts';
 export {
   assertEquals,
   assertNotEquals,
