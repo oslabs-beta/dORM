d1ba1658087570c8e7b52ab741180fd8716f823c
Nick <74734381+nickstillman@users.noreply.github.com>
2021-04-14 22:39:09 -0400

    added updateAll method, double single-quotes inside strings (#23)

--- lib/validate-strings.ts
+++ lib/validate-strings.ts
@@ -1,11 +1,11 @@
 const validate = {
   onWhere: function (str: string): any {
     const tokens = this.tokenize(str);
-
+    
     if (tokens === 'Error') return tokens;
-
+    
     const values: unknown[] = [];
-
+    
     tokens.forEach((token, i) => {
       if (token === 'true') {
         values.push(true);
@@ -20,7 +20,7 @@ const validate = {
         tokens[i] = '$';
       }
       if (token[0] === "'" && token[token.length - 1] === "'") {
-        values.push(token);
+        values.push(token.slice(1, token.length - 1));
         tokens[i] = '$';
       }
       const parsedInt = parseInt(token);
@@ -29,29 +29,29 @@ const validate = {
         tokens[i] = '$';
       }
     });
-
+    
     return { tokens, values };
   },
-
+  
   insertParams: function (tokens: string[], currParam: number) {
     tokens.forEach((token, i) => {
       if (token === '$') tokens[i] = `$${currParam++}`;
     });
     return tokens;
   },
-
+  
   columnsTables: function (arr?: string[] | string | null): boolean {
     if (!arr) return true;
     arr = typeof arr === 'string' ? [arr] : arr;
-
+    
     for (const el of arr) {
       if (typeof el !== 'string') return false;
       if (el.includes('"') || el.includes("'") || el.includes('\\u'))
-        return false;
+      return false;
     }
     return true;
   },
-
+  
   tokenize: function (str: string) {
     let i = 0;
     const tokens: string[] = [];
@@ -59,21 +59,25 @@ const validate = {
     let insideString = false;
     let readyForString = true;
     let escapeNextChar = false;
-
+    
     while (i < str.length) {
       const char = str[i];
-
+      
       if (escapeNextChar) {
-        if (char.toLowerCase() === 'u') return 'Error';
         escapeNextChar = false;
         curr += char;
         i++;
         continue;
       }
-
+      
       if (char === "'") {
         if (!readyForString) {
           return 'Error';
+        }
+        if (str[i + 1] === "'") {
+          curr += "'";
+          i += 2;
+          continue;
         }
         curr += char;
         insideString = !insideString;
@@ -85,28 +89,26 @@ const validate = {
         i++;
         continue;
       }
-
+      
       if (insideString) {
-        if (char === '"') return 'Error';
+        
         if (char === '\\') escapeNextChar = true;
-
+        
         curr += char;
         i++;
         continue;
       }
-
-      if (char === '\\') return 'Error';
-
-      if (char === '"') return 'Error';
-
-      // comparison string below includes tab character at index 1
+      
+      if ('\\"'.includes(char)) return 'Error';
+      
+      // comparison string below contains tab character at index 1
       if (' 	=()'.includes(char)) {
         if (curr.length) {
           if ('=('.includes(char)) readyForString = true;
           tokens.push(curr);
           curr = '';
         }
-
+        
         if ('=()'.includes(char)) tokens.push(char);
       } else {
         curr += char;
@@ -116,7 +118,7 @@ const validate = {
     }
     if (insideString) return 'Error';
     if (curr.length) tokens.push(curr);
-
+    
     return tokens;
   },
 };
